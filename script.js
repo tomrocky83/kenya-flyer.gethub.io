767c728fc0876b6f078de5e21374ca48dc0f68d3
tomrocky83 <50927191+tomrocky83@users.noreply.github.com>
2020-07-17 18:26:44 -0400

    top of facts done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-$(document).ready(function() {});
+document.getElementById('capt').style.display = 'none';
+
+function showCapt() {
+    const img = document.getElementById('capt');
+    img.style.display = '';
+    if (img.style.display == 'inline') {
+        img.style.display = 'none';
+    } else {
+        img.style.display = 'block';
+    }
+}
